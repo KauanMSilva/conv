bd84651b7051a2423e2a0480dd186e50cbe92eae
Kauan Mariano Silva <144391114+KauanMSilva@users.noreply.github.com>
2025-02-13 21:49:12 -0300

    Add files via upload

---
+++ condicionais.js
@@ -0,0 +1,87 @@
+//Exercícios 1
+
+// let num = prompt("Digite um Número :");
+
+// if (num > 0) {
+//     console.log("Positivo");
+// }
+
+// else if (num < 0) {
+//     console.log("Negativo");
+
+// }
+
+// else {
+//     console.log("É Zero");
+// }
+
+//Exercício 2
+
+// let num = prompt("Digite um número aqui");
+
+// if (num % 2 === 0) {
+//     alert("Número é Par");
+// }
+
+// else {
+//     alert("Número é Impar");
+// }
+
+//Exercício 3
+
+// let idade = prompt("Digite um Número :");
+
+//  if (idade<=12) {
+//     console.log("Criança");
+//  }
+
+//  else if (idade<=17) {
+//      console.log("Adolescente");
+
+//  }
+
+//  else {
+//     console.log("Aduto");
+//  }
+
+// Exercício 4
+
+// let nota = prompt("Insira a nota do Aluno");
+
+// if (nota<=59) {
+//     console.log("Reprovado")
+// }
+
+// else if (nota>=60){
+//     console.log("Aprovado")
+// }
+
+
+// Exercício 1 Intermediário
+
+// let a = prompt("Digite um número");
+// let b = prompt("Digite um número");
+// let c = prompt("Digite um número");
+
+// if (a > b && a > c) {
+//     console.log(a);
+// }
+// else if (b > a && b > c) {
+//     console.log(b);
+// } 
+//     else {
+//         console.log(c);
+//     }
+
+
+//Exercício 2 Intermediário
+
+let usuario = prompt("Informe o Usuário!");
+let senha = prompt("Informe sua Senha :");
+if (usuario == "admin" && senha == 1234) {
+    console.log("Login Aceito");
+} 
+else {
+    console.log("Login Recusado");
+}
+
